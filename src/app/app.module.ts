82b4caec4edac9fe1b7b8611679d43e81c74ff35
ThomasApter <33519344+ThomasApter@users.noreply.github.com>
2018-04-25 18:58:58 +0100

    Particlejs and other cosmeticv features

---
+++ src/app/app.module.ts
@@ -0,0 +1,123 @@
+import 'hammerjs';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { ParticlesModule } from 'angular-particle';
+
+import { AppComponent } from './app.component';
+import { ToolbarComponent } from './Public/toolbar/toolbar.component';
+import { PageFooterComponent } from './Public/page-footer/page-footer.component';
+import { HomeComponent } from './MainContent/home/home.component';
+import { AboutComponent } from './MainContent/about/about.component';
+import { StaffComponent } from './MainContent/staff/staff.component';
+import { PageNotFoundComponent } from './Public/page-not-found/page-not-found.component'
+import {
+    MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatPaginatorModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSortModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatFormFieldModule,
+  MatExpansionModule,
+  MatStepperModule
+} from '@angular/material';
+
+
+const appRoutes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent,
+    data: { title: 'About' }
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { title: 'Home' }
+  },
+  {
+    path: 'staff',
+    component: StaffComponent,
+    data: { title: 'Staff' }
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToolbarComponent,
+    PageFooterComponent,
+    HomeComponent,
+    AboutComponent,
+    StaffComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    ParticlesModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSortModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatFormFieldModule,
+    MatExpansionModule,
+    MatStepperModule,
+    RouterModule.forRoot(
+      appRoutes,
+    )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
